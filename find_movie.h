6692a78daa5319359c4cca58cc8e1b8e2fd46b5c
omaralqblawy <omaralqblawy775@gmail.com>
2026-09-06 23:52:48 +0300

    Update find_movie.h

--- find_movie.h
+++ find_movie.h
@@ -3,9 +3,9 @@
 #define FIND_MOVIE_H
 
 #include "cinema_data.h"               // استيراد الهياكل
-using namespace std;                   // لتفادي std::
 
-const Movie* findMovieById(const vector<Movie>& movies, int movieId); // نسخة للقراءة
-Movie* findMovieById(vector<Movie>& movies, int movieId);             // نسخة للتعديل
+
+const Movie* findMovieById(const  std::vector<Movie>& movies, int movieId); // نسخة للقراءة
+Movie* findMovieById(std::vector<Movie>& movies, int movieId);             // نسخة للتعديل
 
 #endif // FIND_MOVIE_H
