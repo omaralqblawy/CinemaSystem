06617d7856a90d5cd16e1451fa3d5defadcb21a6
omaralqblawy <omaralqblawy775@gmail.com>
2026-09-06 22:52:52 +0300

    Update cinema_data.cpp

--- cinema_data.cpp
+++ cinema_data.cpp
@@ -2,13 +2,13 @@
 using namespace std;                  // لتفادي كتابة std:: في كل سطر
 
 // تعريف قائمة الصالات الافتراضية
-vector<Hall> halls = {
+ const vector<Hall> halls = {
     {1, "Main Hall", ROWS_DEFAULT, COLS_DEFAULT, false}, // صالة رئيسية افتراضية
     {2, "VIP Hall", 5, 8, true}                         // صالة VIP افتراضية
 };
 
 // تعريف قائمة الأفلام الابتدائية (مثال)
-vector<Movie> movies = {
+ const vector<Movie> movies = {
     {100, "Inception", "Sci-Fi", "18:30", 5.5, 1, "Now Showing",
         vector<vector<char>>(ROWS_DEFAULT, vector<char>(COLS_DEFAULT, 'O'))} // مقاعد مهيأة
 };
