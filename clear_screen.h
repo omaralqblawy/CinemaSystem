13d26a48cf6e738da22c930d48f3664acb066dc0
omaralqblawy <omaralqblawy775@gmail.com>
2026-09-06 23:27:40 +0300

    Refactor clear_screen.h header guard and formatting

--- clear_screen.h
+++ clear_screen.h
@@ -2,7 +2,7 @@
 #ifndef CLEAR_SCREEN_H                 // حماية من التضمين المتكرر
 #define CLEAR_SCREEN_H
 
-using namespace std;                   // لتفادي std::
+
 
 void clearScreen();                    // إعلان دالة مسح الشاشة
 
