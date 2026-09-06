766ab484bd4f10e0d354e4cf62f4b7f37ddbfa63
omaralqblawy <omaralqblawy775@gmail.com>
2026-09-06 23:54:10 +0300

    Correct pressEnterToContinue function declaration
    Fix function declaration syntax and clean up formatting.

--- press_enter.h
+++ press_enter.h
@@ -2,8 +2,8 @@
 #ifndef PRESS_ENTER_H                  // حماية من التضمين المتكرر
 #define PRESS_ENTER_H
 
-using namespace std;                   // لتفادي std::
 
-void pressEnterToContinue();           // إعلان دالة الانتظار حتى Enter
+
+std::void pressEnterToContinue();           // إعلان دالة الانتظار حتى Enter
 
 #endif // PRESS_ENTER_H
