39b48d6dedd26dcfca5a269f262a5d74fc282f0a
omaralqblawy <omaralqblawy775@gmail.com>
2026-09-07 00:04:12 +0300

    Update print_header.h

--- print_header.h
+++ print_header.h
@@ -3,8 +3,8 @@
 #define PRINT_HEADER_H
 
 #include <string>                       // تضمين string لأن التوقيع يستخدمها
-using namespace std;                   // لتفادي std::
+                  
 
-void printHeader(const string& title);  // إعلان دالة طباعة العنوان
+void printHeader(const std::string& title);  // إعلان دالة طباعة العنوان
 
 #endif // PRINT_HEADER_H
